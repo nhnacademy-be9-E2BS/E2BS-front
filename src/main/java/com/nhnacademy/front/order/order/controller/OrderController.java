a7761835e4a9d0c11666e8a60d812d23bf855c25
JS01C <cjs010131@gmail.com>
2025-05-13 13:50:28 +0900

    feat(Order): 주문 시도를 위한 기본적인 뷰 제작(전체 상품 쿠폰 적용, 필드 검증, POST요청)

--- src/main/java/com/nhnacademy/front/order/order/controller/OrderController.java
+++ src/main/java/com/nhnacademy/front/order/order/controller/OrderController.java
@@ -1,11 +1,27 @@
 package com.nhnacademy.front.order.order.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.nhnacademy.front.order.order.model.dto.request.RequestOrderDTO;
+import com.nhnacademy.front.order.order.model.dto.request.RequestOrderDetailDTO;
+import com.nhnacademy.front.order.order.model.dto.request.RequestOrderWrapperDTO;
+import com.nhnacademy.front.order.order.model.dto.response.ResponseOrderResultDTO;
+import com.nhnacademy.front.order.order.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class OrderController {
+
+	private final OrderService orderService;
+
 	/**
 	 * 결제 주문서 작성 페이지
 	 */
@@ -17,7 +33,10 @@ public class OrderController {
 	}
 
 	@PostMapping("/order")
-	public void postCheckOut() {
+	public ResponseEntity<ResponseOrderResultDTO> postCheckOut(@RequestBody RequestOrderWrapperDTO request) {
+		RequestOrderDTO orderDTO = request.getOrder();
+		List<RequestOrderDetailDTO> orderDetailDTOs = request.getOrderDetails();
 
+		return orderService.createOrder(new RequestOrderWrapperDTO(orderDTO, orderDetailDTOs));
 	}
 }
