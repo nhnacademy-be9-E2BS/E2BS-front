48516971ea60f0b66fe9f7d7b824ab2116a2bb9b
김현규 <khk15888@naver.com>
2025-05-13 20:13:26 +0900

    feat(tagAll, sidebar.html): 태그 테스트 코드를 제외한 기능 구현, sidebar.html에서 불필요한 form 삭제

---
+++ src/main/java/com/nhnacademy/front/product/tag/adaptor/TagAdaptor.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.front.product.tag.adaptor;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.nhnacademy.front.common.page.PageResponse;
+import com.nhnacademy.front.product.tag.model.dto.request.RequestTagDTO;
+import com.nhnacademy.front.product.tag.model.dto.response.ResponseTagDTO;
+
+@FeignClient(name = "tag-service", url = "${product.tag.url}")
+public interface TagAdaptor {
+	@GetMapping
+	ResponseEntity<PageResponse<ResponseTagDTO>> getTags(@SpringQueryMap Pageable pageable);
+
+	@PostMapping
+	ResponseEntity<Void> postCreateTag(@RequestBody RequestTagDTO request);
+
+	@PutMapping("/{tagId}")
+	ResponseEntity<Void> putUpdateTag(@PathVariable Long tagId, @RequestBody RequestTagDTO request);
+}
---
+++ src/test/java/com/nhnacademy/front/product/tag/TagControllerTest.java
@@ -0,0 +1,4 @@
+package com.nhnacademy.front.product.tag;
+
+public class TagControllerTest {
+}
