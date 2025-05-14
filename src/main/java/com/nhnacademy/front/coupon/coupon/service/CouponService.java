b530e286dd96c83f752fee13753519627efec1f4
asdasd7722 <psjune9707@naver.com>
2025-05-14 11:23:27 +0900

    feat(coupon): 관리자 쿠폰 등록 기능
    - 쿠폰정책선택, 쿠폰명입력, 쿠폰종류 선택해서 쿠폰을 등록
    - 카테고리, 상품 구현이 필요해서 테스트 코드는 추후 작성 예정

---
+++ src/main/java/com/nhnacademy/front/coupon/coupon/service/CouponService.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.front.coupon.coupon.service;
+
+import org.springframework.data.domain.Pageable;
+
+import com.nhnacademy.front.common.page.PageResponse;
+import com.nhnacademy.front.coupon.coupon.model.dto.RequestCouponDTO;
+import com.nhnacademy.front.coupon.coupon.model.dto.ResponseCouponDTO;
+
+public interface CouponService {
+	/**
+	 * 관리자 쿠폰 생성
+	 */
+	void createCoupon(RequestCouponDTO request);
+
+	/**
+	 * 관리자 쿠폰 전체 조회
+	 */
+	PageResponse<ResponseCouponDTO> getCoupons(Pageable pageable);
+
+	/**
+	 * 관리자 쿠폰 ID로 단건 조회
+	 */
+	ResponseCouponDTO getCoupon(Long couponId);
+}
