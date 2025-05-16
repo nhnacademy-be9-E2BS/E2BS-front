8051fab93c8c4db7489fb2ee12391ad71e7d997a
K-Y-k <kyk4957@naver.com>
2025-05-16 13:51:45 +0900

    modify(cart): 소나큐브 테스트 통과하도록 수정

--- src/main/java/com/nhnacademy/front/cart/controller/GuestCartController.java
+++ src/main/java/com/nhnacademy/front/cart/controller/GuestCartController.java
@@ -1,8 +1,8 @@
 package com.nhnacademy.front.cart.controller;
 
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +10,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
-import com.nhnacademy.front.account.member.service.MemberService;
 import com.nhnacademy.front.cart.model.dto.request.RequestAddCartItemsDTO;
 import com.nhnacademy.front.cart.model.dto.request.RequestDeleteCartItemsForGuestDTO;
 import com.nhnacademy.front.cart.model.dto.request.RequestUpdateCartItemsDTO;
@@ -24,7 +21,6 @@ import com.nhnacademy.front.cart.model.dto.response.ResponseCartItemsForGuestDTO
 import com.nhnacademy.front.cart.service.GuestCartService;
 import com.nhnacademy.front.common.exception.ValidationFailedException;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
@@ -39,17 +35,16 @@ public class GuestCartController {
 	 * 게스트 장바구니 항목 추가
 	 */
 	@PostMapping("/guests/carts/items")
-	public ResponseEntity<?> guestAddToCart(@Validated @RequestBody RequestAddCartItemsDTO requestDto, BindingResult bindingResult,
+	public ResponseEntity<Void> guestAddToCart(@Validated @RequestBody RequestAddCartItemsDTO requestDto, BindingResult bindingResult,
 		HttpSession httpSession) {
 		if (bindingResult.hasErrors()) {
 			throw new ValidationFailedException(bindingResult);
 		}
 
 		requestDto.setSessionId(httpSession.getId());
-		// requestDto.setSessionId("guest-session-abc123");
 		guestCartService.createCartItemForGuest(requestDto);
 
-		return ResponseEntity.ok().body(Map.of("message", "success"));
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
 	/**
@@ -58,7 +53,6 @@ public class GuestCartController {
 	@GetMapping("/guests/carts")
 	public String getCartsByGuest(Model model, HttpSession httpSession) {
 		String sessionId = httpSession.getId();
-		// String sessionId = "guest-session-abc123";
 		List<ResponseCartItemsForGuestDTO> cartItemsByGuest = guestCartService.getCartItemsByGuest(sessionId);
 
 		long totalPaymentAmount = 0;
@@ -76,46 +70,43 @@ public class GuestCartController {
 	 * 게스트 장바구니 항목 수량 변경
 	 */
 	@PutMapping("/guests/carts/items")
-	public ResponseEntity<?> updateCartItemForGuest(@Validated @RequestBody RequestUpdateCartItemsDTO requestDto, BindingResult bindingResult,
+	public ResponseEntity<Void> updateCartItemForGuest(@Validated @RequestBody RequestUpdateCartItemsDTO requestDto, BindingResult bindingResult,
 													HttpSession httpSession) {
 		if (bindingResult.hasErrors()) {
 			throw new ValidationFailedException(bindingResult);
 		}
 
 		requestDto.setSessionId(httpSession.getId());
-		// requestDto.setSessionId("guest-session-abc123");
 		guestCartService.updateCartItemForGuest(requestDto);
 
-		return ResponseEntity.ok().body(Map.of("message", "success"));
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 	/**
 	 * 게스트 장바구니 항목 삭제
 	 */
 	@DeleteMapping("/guests/carts/items")
-	public ResponseEntity<?> deleteCartItemForGuest(@Validated @RequestBody RequestDeleteCartItemsForGuestDTO requestDto, BindingResult bindingResult,
+	public ResponseEntity<Void> deleteCartItemForGuest(@Validated @RequestBody RequestDeleteCartItemsForGuestDTO requestDto, BindingResult bindingResult,
 													HttpSession httpSession) {
 		if (bindingResult.hasErrors()) {
 			throw new ValidationFailedException(bindingResult);
 		}
 
 		requestDto.setSessionId(httpSession.getId());
-		// requestDto.setSessionId("guest-session-abc123");
 		guestCartService.deleteCartItemForGuest(requestDto);
 
-		return ResponseEntity.ok().body(Map.of("message", "success"));
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 	/**
 	 * 게스트 장바구니 전체 삭제
 	 */
 	@DeleteMapping("/guests/carts")
-	public ResponseEntity<?> deleteCartForCustomer(HttpSession httpSession) {
+	public ResponseEntity<Void> deleteCartForCustomer(HttpSession httpSession) {
 		String sessionId = httpSession.getId();
-		// String sessionId = "guest-session-abc123";
 		guestCartService.deleteCartForGuest(sessionId);
 
-		return ResponseEntity.ok().body(Map.of("message", "success"));
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 }
