4e25cba3c3d274eca9561bdccf1fb9b18c276b32
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-19 22:56:32 +0900

    feat(CustomAuthenticationSuccessHandler): url에 따른 로그인 기능 추가

--- src/main/java/com/nhnacademy/front/common/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/nhnacademy/front/common/handler/CustomAuthenticationSuccessHandler.java
@@ -3,6 +3,7 @@ package com.nhnacademy.front.common.handler;
 import java.io.IOException;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import com.nhnacademy.front.account.auth.model.dto.request.RequestJwtTokenDTO;
@@ -22,7 +23,7 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 		HttpServletResponse response,
 		Authentication authentication) throws IOException {
 
-		String url = request.getRequestURI();
+		String url = request.getHeader("Referer"); // 브라우저의 URL 주소를 가져오기 위한 Referer 사용
 		String memberId = authentication.getName();
 
 		boolean isAdmin = authentication.getAuthorities().stream()
@@ -31,18 +32,18 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 		boolean isMember = authentication.getAuthorities().stream()
 			.anyMatch(authority -> authority.getAuthority().equals("ROLE_MEMBER"));
 
-		if (isAdmin) {
+		if (isAdmin && !url.contains("/admin/login")) {
+			SecurityContextHolder.clearContext(); // Security 초기화
+			response.sendRedirect("/login");
+		} else if (isMember && url.contains("/admin/login")) {
+			SecurityContextHolder.clearContext(); // Security 초기화
+			response.sendRedirect("/login");
+		} else {
+			RequestJwtTokenDTO requestJwtTokenDTO = new RequestJwtTokenDTO(memberId);
+			authService.postAuthCreateJwtToken(requestJwtTokenDTO, response, request);
 
+			response.sendRedirect("/index");
 		}
-
-		if (isMember) {
-
-		}
-
-		RequestJwtTokenDTO requestJwtTokenDTO = new RequestJwtTokenDTO(memberId);
-		authService.postAuthCreateJwtToken(requestJwtTokenDTO, response, request);
-
-		response.sendRedirect("/index");
 	}
 
 }
