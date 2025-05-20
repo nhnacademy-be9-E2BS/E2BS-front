143505b5d4ff22da55c891e793b74b01037c322e
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-20 17:49:29 +0900

    feat(MemberMypageController): 마이페이지 구현

--- src/main/java/com/nhnacademy/front/account/member/controller/mypage/MemberMypageController.java
+++ src/main/java/com/nhnacademy/front/account/member/controller/mypage/MemberMypageController.java
@@ -1,15 +1,59 @@
 package com.nhnacademy.front.account.member.controller.mypage;
 
+import java.util.Objects;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.nhnacademy.front.account.member.model.dto.request.RequestMemberIdDTO;
+import com.nhnacademy.front.account.member.service.MemberMypageService;
+import com.nhnacademy.front.account.member.service.MemberService;
+import com.nhnacademy.front.common.annotation.JwtTokenCheck;
+import com.nhnacademy.front.jwt.parser.JwtGetMemberId;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.BadRequestException;
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/mypage")
 public class MemberMypageController {
 
+	private final MemberService memberService;
+	private final MemberMypageService memberMypageService;
+
+	@JwtTokenCheck
 	@GetMapping
-	public String getMypage() {
+	public String getMypage(HttpServletRequest request,
+		Model model) {
+
+		String memberId = JwtGetMemberId.jwtGetMemberId(request);
+		if (Objects.isNull(memberId)) {
+			throw new BadRequestException("다시 로그인 해주세요.");
+		}
+
+		RequestMemberIdDTO requestMemberIdDTO = new RequestMemberIdDTO(memberId);
+		String memberName = memberService.getMemberName(request);
+
+		int couponCnt = 0;
+		int couponCntFromBack = memberMypageService.getMemberCoupon(requestMemberIdDTO);
+		if (couponCntFromBack > 0) {
+			couponCnt = couponCntFromBack;
+		}
+
+		long pointAmount = 0;
+		long pointAmountFromBack = memberMypageService.getMemberPoint(requestMemberIdDTO);
+		if (pointAmountFromBack > 0) {
+			pointAmount = pointAmountFromBack;
+		}
+
+		model.addAttribute("memberName", memberName);
+		model.addAttribute("couponCnt", couponCnt);
+		model.addAttribute("pointAmount", pointAmount);
+
 		return "member/mypage/mypage";
 	}
 
---
+++ src/main/java/com/nhnacademy/front/account/member/service/MemberMypageService.java
@@ -0,0 +1,44 @@
+package com.nhnacademy.front.account.member.service;
+
+import java.util.Objects;
+
+import org.springframework.stereotype.Service;
+
+import com.nhnacademy.front.account.member.adaptor.MemberCouponAdaptor;
+import com.nhnacademy.front.account.member.adaptor.MemberPointHistoryAdaptor;
+import com.nhnacademy.front.account.member.model.dto.request.RequestMemberIdDTO;
+import com.nhnacademy.front.account.member.model.dto.response.ResponseMemberCouponDTO;
+import com.nhnacademy.front.account.member.model.dto.response.ResponseMemberPointDTO;
+import com.nhnacademy.front.common.exception.EmptyResponseException;
+
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberMypageService {
+
+	private final MemberCouponAdaptor memberCouponAdaptor;
+	private final MemberPointHistoryAdaptor memberPointHistoryAdaptor;
+
+	public int getMemberCoupon(RequestMemberIdDTO requestMemberIdDTO) throws FeignException {
+		ResponseMemberCouponDTO responseMemberCouponDTO = memberCouponAdaptor
+			.getMemberCouponAmount(requestMemberIdDTO.getMemberId());
+		if (Objects.isNull(responseMemberCouponDTO)) {
+			throw new EmptyResponseException("쿠폰 개수를 가져오지 못했습니다.");
+		}
+
+		return responseMemberCouponDTO.getCouponCnt();
+	}
+
+	public long getMemberPoint(RequestMemberIdDTO requestMemberIdDTO) throws FeignException {
+		ResponseMemberPointDTO responseMemberPointDTO = memberPointHistoryAdaptor
+			.getMemberPointAmount(requestMemberIdDTO.getMemberId());
+		if (Objects.isNull(responseMemberPointDTO)) {
+			throw new EmptyResponseException("포인트 정보를 가져오지 못했습니다.");
+		}
+
+		return responseMemberPointDTO.getPointAmount();
+	}
+
+}
