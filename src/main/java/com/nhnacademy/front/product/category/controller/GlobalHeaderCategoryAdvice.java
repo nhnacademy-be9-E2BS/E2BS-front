90ed06dbb0404b2b3656a1fc7da3f344e7c077a8
이영채 <dudco5858@naver.com>
2025-05-22 19:02:04 +0900

    feat(category): GlobalHeaderCategoryAdvice 코드 수정

--- src/main/java/com/nhnacademy/front/product/category/controller/GlobalHeaderCategoryAdvice.java
+++ src/main/java/com/nhnacademy/front/product/category/controller/GlobalHeaderCategoryAdvice.java
@@ -6,18 +6,23 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.front.product.category.model.dto.response.ResponseCategoryDTO;
 import com.nhnacademy.front.product.category.service.UserCategoryService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
 @RequiredArgsConstructor
+@Slf4j
 public class GlobalHeaderCategoryAdvice {
 
 	private final UserCategoryService userCategoryService;
 	private final RedisTemplate<String, Object> redisTemplate;
+	private final ObjectMapper objectMapper;
 
 	@ModelAttribute("headerCategories")
 	public List<ResponseCategoryDTO> getHeaderCategories(HttpServletRequest request) {
@@ -30,7 +35,10 @@ public class GlobalHeaderCategoryAdvice {
 			Object cached = redisTemplate.opsForValue().get("Categories::header");
 			if (cached instanceof List<?>) {
 				@SuppressWarnings("unchecked")
-				List<ResponseCategoryDTO> categoryDTOs = (List<ResponseCategoryDTO>) cached;
+				List<ResponseCategoryDTO> categoryDTOs = objectMapper.convertValue(cached,
+					new TypeReference<List<ResponseCategoryDTO>>() {});
+				log.info("캐시");
+				log.info(categoryDTOs + "");
 				return categoryDTOs;
 			}
 		}
