312c9a9021288a174cc4ebeb6177b78f2bbd6aa6
JS01C <cjs010131@gmail.com>
2025-05-22 11:05:57 +0900

    hotfix(Security, Interceptor): /index 경로 삭제 및 /로 통일, 쿠키 없을 시 특정 경로 예외 제거

--- src/main/java/com/nhnacademy/front/common/interceptor/FeignCookieInterceptor.java
+++ src/main/java/com/nhnacademy/front/common/interceptor/FeignCookieInterceptor.java
@@ -40,7 +40,7 @@ public class FeignCookieInterceptor implements RequestInterceptor {
 
 			Cookie[] cookies = request.getCookies();
 			if (cookies == null || cookies.length == 0) {
-				throw new LoginRedirectException("로그인을 다시 해주세요");
+				return;
 			}
 
 			for (Cookie cookie : cookies) {
@@ -49,6 +49,11 @@ public class FeignCookieInterceptor implements RequestInterceptor {
 					break;
 				}
 			}
+
+			if (cookieHeaders.toString().isEmpty()) {
+				throw new LoginRedirectException("로그인을 다시 해주세요");
+			}
+
 			log.info("FeignCookieInterceptor:{}", cookieHeaders.toString());
 
 			requestTemplate.header(JwtRule.JWT_ISSUE_HEADER.getValue(), cookieHeaders.toString());
