e8aba607195bd2e0d4078aabedea4c092fedc306
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-27 12:28:49 +0900

    feat(AdminSettingsController): 비회원 목록 조회를 위한 기능 추가

--- src/main/java/com/nhnacademy/front/account/admin/adaptor/AdminSettingsAdaptor.java
+++ src/main/java/com/nhnacademy/front/account/admin/adaptor/AdminSettingsAdaptor.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.front.account.admin.adaptor;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.nhnacademy.front.account.admin.model.dto.request.RequestAdminSettingsMemberStateDTO;
 import com.nhnacademy.front.account.admin.model.dto.response.ResponseAdminSettingsDTO;
 import com.nhnacademy.front.account.admin.model.dto.response.ResponseAdminSettingsMembersDTO;
+import com.nhnacademy.front.account.admin.model.dto.response.ResponseAdminSettingsNonMembersDTO;
 import com.nhnacademy.front.common.page.PageResponse;
 
 @FeignClient(name = "admin-settings-adaptor", url = "${admin.settings.url}")
@@ -21,7 +24,8 @@ public interface AdminSettingsAdaptor {
 	ResponseEntity<ResponseAdminSettingsDTO> getAdminSettings();
 
 	@GetMapping("/members")
-	ResponseEntity<PageResponse<ResponseAdminSettingsMembersDTO>> getAdminSettingsMembers();
+	ResponseEntity<PageResponse<ResponseAdminSettingsMembersDTO>> getAdminSettingsMembers(
+		@SpringQueryMap Pageable pageable);
 
 	@PostMapping("/members/{memberId}")
 	ResponseEntity<Void> updateAdminSettingsMemberState(@PathVariable("memberId") String memberId,
@@ -32,5 +36,9 @@ public interface AdminSettingsAdaptor {
 
 	@DeleteMapping("/members/{memberId}")
 	ResponseEntity<Void> deleteAdminSettingsMember(@PathVariable("memberId") String memberId);
+
+	@GetMapping("/non-members")
+	ResponseEntity<PageResponse<ResponseAdminSettingsNonMembersDTO>> getAdminSettingsNonMembers(
+		@SpringQueryMap Pageable pageable);
 
 }
