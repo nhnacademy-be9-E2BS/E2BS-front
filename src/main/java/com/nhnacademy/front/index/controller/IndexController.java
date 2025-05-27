81abb1d9de88fc9d1d6334bf97458229e836b1bc
Suin Park <tndls2490@o.cnu.ac.kr>
2025-05-27 11:02:57 +0900

    modify(product, tag) : url 수정, 어노테이션 추가

--- src/main/java/com/nhnacademy/front/index/controller/IndexController.java
+++ src/main/java/com/nhnacademy/front/index/controller/IndexController.java
@@ -1,31 +1,28 @@
-package com.nhnacademy.front.index.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.nhnacademy.front.account.member.service.MemberService;
-import com.nhnacademy.front.common.annotation.JwtTokenCheck;
-import com.nhnacademy.front.index.service.IndexService;
-
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequiredArgsConstructor
-public class IndexController {
-
-	private final IndexService indexService;
-	private final MemberService memberService;
-
-	@GetMapping("/")
-	@JwtTokenCheck
-	public String getIndex(Model model) {
-		model.addAttribute("BestSellerList", indexService.getBestSellerProducts());
-		model.addAttribute("BlogBestList", indexService.getBlogBestProducts());
-		model.addAttribute("ItemNewAllList", indexService.getNewItemsProducts());
-		model.addAttribute("NewSpecialItemsList", indexService.getNewSpecialItemsProducts());
-		model.addAttribute("memberName", "회원 이름");
-
-		return "home";
-	}
-}
+// package com.nhnacademy.front.index.controller;
+//
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// //
+// // import com.nhnacademy.front.common.annotation.JwtTokenCheck;
+// // import com.nhnacademy.front.index.service.IndexService;
+// //
+// // import lombok.RequiredArgsConstructor;
+// //
+// // @Controller
+// // @RequiredArgsConstructor
+// // public class IndexController {
+// //
+// // 	private final IndexService indexService;
+// //
+// // 	@GetMapping("/")
+// // 	@JwtTokenCheck
+// // 	public String getIndex(Model model) {
+// // 		model.addAttribute("BestSellerList", indexService.getBestSellerProducts());
+// // 		model.addAttribute("BlogBestList", indexService.getBlogBestProducts());
+// // 		model.addAttribute("ItemNewAllList", indexService.getNewItemsProducts());
+// // 		model.addAttribute("NewSpecialItemsList", indexService.getNewSpecialItemsProducts());
+// // 		model.addAttribute("memberName", "회원 이름");
+// //
+// // 		return "home";
+// // 	}
+// // }
