364259e95d88f950c917806f496afc8b5004a854
JS01C <cjs010131@gmail.com>
2025-05-28 15:54:53 +0900

    modify(Wrapper): 포장지 관리자 페이지 접근 확인을 위한 임의 경로 수정

--- src/test/java/com/nhnacademy/front/order/wrapper/WrapperControllerTest.java
+++ src/test/java/com/nhnacademy/front/order/wrapper/WrapperControllerTest.java
@@ -82,7 +82,7 @@ class WrapperControllerTest {
 		Mockito.when(wrapperService.getWrappers(any())).thenReturn(pageResponse);
 
 		// when & then
-		mockMvc.perform(get("/admin/settings/wrappers"))
+		mockMvc.perform(get("/admin/settings/wrappers/temp"))
 			.andExpect(status().isOk())
 			.andExpect(view().name("admin/product/wrappers"))
 			.andExpect(model().attributeExists("wrappers"));
