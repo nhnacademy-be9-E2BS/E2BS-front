7ba160ebfc240fb7ffdc49dc4a29fc6ecbf88436
K-Y-k <kyk4957@naver.com>
2025-05-29 13:22:02 +0900

    feat(like): 좋아요 추가/취소/좋아요 상품목록/좋아요 개수 뷰 구현 완료 및 테스트 코드 작성

--- src/main/java/com/nhnacademy/front/product/like/controller/LikeController.java
+++ src/main/java/com/nhnacademy/front/product/like/controller/LikeController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,14 +23,12 @@ import com.nhnacademy.front.product.like.model.dto.request.RequestCreateLikeDTO;
 import com.nhnacademy.front.product.like.model.dto.response.ResponseLikedProductDTO;
 import com.nhnacademy.front.product.like.service.LikeService;
 
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class LikeController {
-	private static final String JWT_TOKEN_NULL_MESSAGE = "JWT token is null";
 
 	private final LikeService likeService;
 
@@ -42,15 +41,12 @@ public class LikeController {
 		}
 
 		String memberId = JwtGetMemberId.jwtGetMemberId(request);
-		if (Objects.isNull(memberId)) {
-			throw new JwtException(JWT_TOKEN_NULL_MESSAGE);
-		}
 
 		RequestCreateLikeDTO requestDto = new RequestCreateLikeDTO();
 		requestDto.setMemberId(memberId);
 
 		likeService.createLike(productId, requestDto);
-		return  ResponseEntity.ok().build();
+		return  ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
 	@JwtTokenCheck
@@ -61,9 +57,6 @@ public class LikeController {
 		}
 
 		String memberId = JwtGetMemberId.jwtGetMemberId(request);
-		if (Objects.isNull(memberId)) {
-			throw new JwtException(JWT_TOKEN_NULL_MESSAGE);
-		}
 
 		likeService.deleteLike(productId, memberId);
 		return ResponseEntity.ok().build();
@@ -73,9 +66,6 @@ public class LikeController {
 	@GetMapping("/products/likes")
 	public String getLikedProductsByCustomer(@PageableDefault(size = 6, sort = "likeCreatedAt", direction = Sort.Direction.DESC) Pageable pageable, HttpServletRequest request, Model model) {
 		String memberId = JwtGetMemberId.jwtGetMemberId(request);
-		if (Objects.isNull(memberId)) {
-			throw new JwtException(JWT_TOKEN_NULL_MESSAGE);
-		}
 
 		PageResponse<ResponseLikedProductDTO> response = likeService.getLikeProductsByCustomer(memberId, pageable);
 		Page<ResponseLikedProductDTO> likeProductsByCustomer = PageResponseConverter.toPage(response);
---
+++ src/test/java/com/nhnacademy/front/product/like/service/LikeServiceTest.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.front.product.like.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class LikeServiceTest {
+
+
+
+}
