01d1e6666ec397af7faf776715b3b864b53d71fe
asdasd7722 <psjune9707@naver.com>
2025-05-29 17:43:13 +0900

    feat(point history): 포인트 내역 조회, 회원가입 포인트 적립 기능 구현

---
+++ src/main/java/com/nhnacademy/front/account/pointhistory/adaptor/PointHistoryAdaptor.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.front.account.pointhistory.adaptor;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.nhnacademy.front.account.pointhistory.model.dto.response.ResponsePointHistoryDTO;
+import com.nhnacademy.front.common.page.PageResponse;
+
+@FeignClient(name = "point-history-service", url = "${auth.member.mypage.url}")
+public interface PointHistoryAdaptor {
+
+	@GetMapping("/{memberId}/pointHistory")
+	ResponseEntity<PageResponse<ResponsePointHistoryDTO>> getPointHistoryByMemberId(@PathVariable("memberId") String memberId, Pageable pageable);
+}
