8b52b73ec4cdca10856a9ed15b6cd256e37f6570
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-30 09:06:34 +0900

    feat(OAuth): PAYCO OAuth 기능 구현

--- src/main/java/com/nhnacademy/front/account/oauth/service/OAuthService.java
+++ src/main/java/com/nhnacademy/front/account/oauth/service/OAuthService.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.nhnacademy.front.account.auth.model.dto.request.RequestJwtTokenDTO;
+import com.nhnacademy.front.account.auth.service.AuthService;
 import com.nhnacademy.front.account.oauth.adaptor.OAuthLoginAdaptor;
 import com.nhnacademy.front.account.oauth.adaptor.OAuthPaycoMemberInfoAdaptor;
 import com.nhnacademy.front.account.oauth.adaptor.OAuthProviderPaycoAccessTokenAdaptor;
 import com.nhnacademy.front.account.oauth.adaptor.OAuthRegisterAdaptor;
 import com.nhnacademy.front.account.oauth.exception.PaycoProcessingException;
-import com.nhnacademy.front.account.oauth.model.dto.request.RequestOAuthPaycoMemberInfoDTO;
 import com.nhnacademy.front.account.oauth.model.dto.request.RequestOAuthRegisterDTO;
 import com.nhnacademy.front.account.oauth.model.dto.response.ResponseCheckOAuthIdDTO;
 import com.nhnacademy.front.account.oauth.model.dto.response.ResponsePaycoMemberInfoDTO;
@@ -19,8 +20,12 @@ import com.nhnacademy.front.account.oauth.model.dto.response.ResponseProviderPay
 import com.nhnacademy.front.common.exception.ServerErrorException;
 
 import feign.FeignException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OAuthService {
@@ -32,8 +37,19 @@ public class OAuthService {
 	private final OAuthLoginAdaptor oAuthLoginAdaptor;
 	private final OAuthRegisterAdaptor oAuthRegisterAdaptor;
 
+	private final AuthService authService;
+
+	@Value("${spring.security.oauth2.client.provider.payco.authorization-uri}")
+	private String authorizationUri;
+
+	@Value("${spring.security.oauth2.client.registration.payco.redirect-uri}")
+	private String redirectUri;
+
+	@Value("${spring.security.oauth2.client.registration.payco.scope}")
+	private String scope;
+
 	@Value("${spring.security.oauth2.client.registration.payco.authorization-grant-type}")
-	private String authorization_grant_type;
+	private String authorizationGrantType;
 
 	@Value("${spring.security.oauth2.client.registration.payco.client-id}")
 	private String clientId;
@@ -41,9 +57,19 @@ public class OAuthService {
 	@Value("${spring.security.oauth2.client.registration.payco.client-secret}")
 	private String clientSecret;
 
+	public String getPaycoLogin() {
+		String requestPath = authorizationUri;
+		requestPath += "&response_type=code";
+		requestPath += "&client_id=" + clientId;
+		requestPath += "&scope=" + scope;
+		requestPath += "&redirect_uri=" + redirectUri;
+
+		return requestPath;
+	}
+
 	public ResponseProviderPaycoAccessTokenDTO getPaycoAccessToken(String code) {
 		ResponseEntity<ResponseProviderPaycoAccessTokenDTO> response = oAuthProviderPaycoAccessTokenAdaptor.getPaycoAccessToken(
-			authorization_grant_type, clientId, clientSecret, code
+			authorizationGrantType, clientId, clientSecret, code
 		);
 		if (!response.getStatusCode().is2xxSuccessful() || Objects.isNull(response.getBody())) {
 			throw new PaycoProcessingException(PAYCO_PROCESSING_MESSAGE);
@@ -53,22 +79,25 @@ public class OAuthService {
 	}
 
 	public ResponsePaycoMemberInfoDTO getPaycoMemberInfo(String accessToken) {
-		RequestOAuthPaycoMemberInfoDTO requestOAuthPaycoMemberInfoDTO = new RequestOAuthPaycoMemberInfoDTO(
-			clientId, accessToken
-		);
-
 		ResponseEntity<ResponsePaycoMemberInfoDTO> response =
-			oAuthPaycoMemberInfoAdaptor.getPaycoMemberInfo(requestOAuthPaycoMemberInfoDTO);
-		if (!response.getStatusCode().is2xxSuccessful() || Objects.isNull(response.getBody())) {
+			oAuthPaycoMemberInfoAdaptor.getPaycoMemberInfo(clientId, accessToken);
+
+		if (!response.getStatusCode().is2xxSuccessful()
+			|| response.getBody() == null
+			|| response.getBody().getData() == null
+			|| response.getBody().getData().getMember() == null) {
+
 			throw new PaycoProcessingException(PAYCO_PROCESSING_MESSAGE);
 		}
 
 		return response.getBody();
 	}
 
-	public void paycoLogin(ResponsePaycoMemberInfoDTO responsePaycoMemberInfoDTO) {
+	public void paycoLogin(ResponsePaycoMemberInfoDTO responsePaycoMemberInfoDTO,
+		HttpServletRequest request,
+		HttpServletResponse response) {
 		try {
-			String idNo = responsePaycoMemberInfoDTO.getIdNo();
+			String idNo = responsePaycoMemberInfoDTO.getData().getMember().getIdNo();
 
 			ResponseEntity<ResponseCheckOAuthIdDTO> responseCheckOAuthIdDTO = oAuthLoginAdaptor.checkOAuthLoginId(idNo);
 			if (!responseCheckOAuthIdDTO.getStatusCode().is2xxSuccessful()
@@ -78,11 +107,11 @@ public class OAuthService {
 
 			if (!responseCheckOAuthIdDTO.getBody().isExistsOAuthId()) {
 				RequestOAuthRegisterDTO requestOAuthRegisterDTO = RequestOAuthRegisterDTO.builder()
-					.memberId(responsePaycoMemberInfoDTO.getIdNo())
-					.email(responsePaycoMemberInfoDTO.getEmail())
-					.mobile(responsePaycoMemberInfoDTO.getMobile())
-					.name(responsePaycoMemberInfoDTO.getName())
-					.birthdayMMdd(responsePaycoMemberInfoDTO.getBirthdayMMdd())
+					.memberId(responsePaycoMemberInfoDTO.getData().getMember().getIdNo())
+					.email(responsePaycoMemberInfoDTO.getData().getMember().getEmail())
+					.mobile(responsePaycoMemberInfoDTO.getData().getMember().getMobile())
+					.name(responsePaycoMemberInfoDTO.getData().getMember().getName())
+					.birthdayMMdd(responsePaycoMemberInfoDTO.getData().getMember().getBirthdayMMdd())
 					.build();
 
 				ResponseEntity<Void> registerResponse = oAuthRegisterAdaptor.registerOAuth(requestOAuthRegisterDTO);
@@ -90,6 +119,10 @@ public class OAuthService {
 					throw new ServerErrorException(SYSTEM_ERROR_MESSAGE);
 				}
 			}
+
+			RequestJwtTokenDTO requestJwtTokenDTO = new RequestJwtTokenDTO(
+				responsePaycoMemberInfoDTO.getData().getMember().getIdNo());
+			authService.postAuthCreateJwtToken(requestJwtTokenDTO, response, request);
 
 		} catch (FeignException ex) {
 			throw new ServerErrorException(SYSTEM_ERROR_MESSAGE);
