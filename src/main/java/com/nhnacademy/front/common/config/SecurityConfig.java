f52e8024ae95be6a5c9ad89c606bd57a5a33b394
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-30 16:43:52 +0900

    modify(sonarqube): sonarqube를 위한 수정

--- src/main/java/com/nhnacademy/front/common/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/front/common/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,13 +42,9 @@ public class SecurityConfig {
 		CustomAuthenticationFailureHandler customAuthenticationFailureHandler = new CustomAuthenticationFailureHandler();
 
 		/**
-		 *  CSRF 보호 기능을 비활성화
+		 *  권한있는 회원만 접근 가능한 URL 설정
 		 */
-		http.csrf(AbstractHttpConfigurer::disable)
-			/**
-			 *  권한있는 회원만 접근 가능한 URL 설정
-			 */
-			.authorizeHttpRequests(auth -> auth
+		http.authorizeHttpRequests(auth -> auth
 				.requestMatchers(LOGIN_URL, ROOT_URL, REGISTER_URL, ADMIN_LOGIN_URL, ACTUATOR_HEALTH).permitAll()
 				.requestMatchers("/css/**", "/js/**", "/img/**", "/fonts/**", "/scss/**", "/vendors/**",
 					"/Aroma Shop-doc/**").permitAll()
