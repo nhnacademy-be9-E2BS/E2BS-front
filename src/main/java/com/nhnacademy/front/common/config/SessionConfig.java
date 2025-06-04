b21695944edb17d3d40eeecd41acf43bc3c14c45
K-Y-k <kyk4957@naver.com>
2025-06-04 14:59:44 +0900

    feat(session-cluster): 세션 클러스터링 테스트

--- src/main/java/com/nhnacademy/front/common/config/SessionConfig.java
+++ src/main/java/com/nhnacademy/front/common/config/SessionConfig.java
@@ -1,13 +1,13 @@
-// package com.nhnacademy.front.common.config;
-//
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-//
-// @Configuration
-// @EnableRedisHttpSession   // Redis를 세션 저장소로 사용
-// public class SessionConfig {
-//
-// 	// 내부에 아무 것도 정의하지 않아도 됨
-// 	// application.yml의 spring.data.redis. 설정이 자동으로 적용
-//
-// }
+package com.nhnacademy.front.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession   // Redis를 세션 저장소로 사용
+public class SessionConfig {
+
+	// 내부에 아무 것도 정의하지 않아도 됨
+	// application.yml의 spring.data.redis. 설정이 자동으로 적용
+
+}
