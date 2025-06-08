a2dac844fd856847fad21f04a6702d7271b7938c
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-06-09 00:20:40 +0900

    feat(swagger): front swagger 적용

--- src/main/java/com/nhnacademy/front/account/member/controller/login/MemberLoginController.java
+++ src/main/java/com/nhnacademy/front/account/member/controller/login/MemberLoginController.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 
+@Tag(name = "로그인", description = "로그인 페이지 및 기능 제공")
 @Controller
 @RequestMapping("/login")
 public class MemberLoginController {
@@ -13,6 +16,7 @@ public class MemberLoginController {
 	/**
 	 * 로그인 뷰
 	 */
+	@Operation(summary = "로그인 폼 페이지", description = "로그인 화면 제공")
 	@GetMapping
 	public String getLogin(HttpServletRequest request) {
 		if (request.getSession().getAttribute("dormantCnt") != null) {
