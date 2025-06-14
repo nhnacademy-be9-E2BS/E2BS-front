591b979edb30fd90ed16a1b88c62f2a26e9b0d88
Kim-Yong-Kyeong <kyk4957@naver.com>
2025-06-15 03:27:18 +0900

    sonarcube(issue): 소나큐브 중첩 이슈 해결

--- src/main/java/com/nhnacademy/front/common/interceptor/CartInterceptor.java
+++ src/main/java/com/nhnacademy/front/common/interceptor/CartInterceptor.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.front.common.interceptor;
 
-import java.util.List;
 import java.util.Objects;
 
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -11,24 +10,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class CartInterceptor implements HandlerInterceptor {
 
-	private List<String> excludeUrls = List.of("/members/order", "/customers/order", "/order/payment", "/order/point", "/order/success", "/order/auth", "/order/confirm");
 	private static final String TARGET_COOKIE_NAME = "orderCart";
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
-		String requestURI = request.getRequestURI();
-
-		if (!excludeUrls.contains(requestURI)) {
-			Cookie[] cookies = request.getCookies();
-			if (Objects.nonNull(cookies)) {
-				for (Cookie cookie : cookies) {
-					if (TARGET_COOKIE_NAME.equals(cookie.getName())) {
-						cookie.setValue("");
-						cookie.setPath("/");
-						cookie.setMaxAge(0);
-						response.addCookie(cookie);
-					}
+		Cookie[] cookies = request.getCookies();
+		if (Objects.nonNull(cookies)) {
+			for (Cookie cookie : cookies) {
+				if (TARGET_COOKIE_NAME.equals(cookie.getName())) {
+					cookie.setValue("");
+					cookie.setPath("/");
+					cookie.setMaxAge(0);
+					response.addCookie(cookie);
 				}
 			}
 		}
