6b93dcf6ea335d4240e035e81b9dcd2a6c674e8a
김현규 <khk15888@naver.com>
2025-06-16 14:29:00 +0900

    추천도서

--- src/main/java/com/nhnacademy/front/product/product/adaptor/ProductAdaptor.java
+++ src/main/java/com/nhnacademy/front/product/product/adaptor/ProductAdaptor.java
@@ -14,8 +14,13 @@ import com.nhnacademy.front.product.product.model.dto.response.ResponseProductRe
 public interface ProductAdaptor {
 
 	@GetMapping("/api/books/{book-id}")
-	ResponseEntity<ResponseProductReadDTO> getProductById(@PathVariable("book-id") Long bookId, @RequestParam("memberId") String memberId);
+	ResponseEntity<ResponseProductReadDTO> getProductById(@PathVariable("book-id") Long bookId,
+		@RequestParam("memberId") String memberId);
 
 	@GetMapping("/api/books/order")
 	ResponseEntity<List<ResponseProductReadDTO>> getProducts(@RequestParam("products") List<Long> products);
+
+	@GetMapping("/api/books/{book-id}/recommend-books")
+	ResponseEntity<List<ResponseProductReadDTO>> getRecommendedProducts(@PathVariable("book-id") Long bookId,
+		@RequestParam("memberId") String memberId);
 }
